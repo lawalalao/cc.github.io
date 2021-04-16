2be6cb5b2faad638dd1d4120306b0a968553c17d
lawalalao <lawalalaoad@gmail.com>
2021-04-16 17:00:44 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$('.test-slider').slick({
+    slidesToShow: 2,
+    dots: false,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+});
